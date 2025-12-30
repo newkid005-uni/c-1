e23a21bf5000d955dd55aa1813d2fac57c092af6
newkid005-uni <newkid005@gmail.com>
2025-12-30 21:44:14 +0900

    2025.12.30 시퀀스 포인터

--- c-2/Program_1/Source.c
+++ c-2/Program_1/Source.c
@@ -31,6 +31,28 @@ void swap(int* argumentX, int* argumentY)
 	
 }
 
+int abs(int a)
+{
+	return(a < 0) ? -a : a;
+}
+
+void function(int x)
+{
+	if (x <= 0)
+	{
+		return;
+	}
+
+	printf("%d\n", x);
+
+	function(--x);
+}
+
+inline int square(b)
+{
+	return b * b;
+}
+
 void main()
 {
 #pragma region 함수
@@ -63,12 +85,12 @@ void main()
 #pragma region 인수
 	//함수가 호출될 때 매개 변수에 실제로 전달되는 값입니다.
 	
-	int x = 10;
-	int y = 20;
-
-	swap(&x, &y);
-
-	printf("x : %d\ny : %d\n", x, y);
+	//int x = 10;
+	//int y = 20;
+	//
+	//swap(&x, &y);
+	//
+	//printf("x : %d\ny : %d\n", x, y);
 	
 	//인수의 경우 함수에 있는 매개 변수에 따라 전달할 수 있는
 	//인수의 수가 결정되며, 값을 전달하는 인수와 값을 전달 받는
@@ -77,7 +99,39 @@ void main()
 	
 #pragma endregion
 
+#pragma region 반환형
+	//함수가 실행을 마치고, 호출한 쪽으로 어떤 자료형의 값을
+	//반환할 지 미리 알려주는 형식입니다.
 
+	//printf("%d", abs(10));
+
+	//함수의 경우 자료형과 반환하는 값의 형태가
+	//일치하지 않으면 원하는 값을 얻을 수 없습니다.
+#pragma endregion
+
+#pragma region 재귀 함수
+	//어떤 함수에서 자신을 다시 호출하여 작업을
+	//수행하는 함수입니다.
+
+	//function(3);
+
+	//재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	//메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생합니다.
+
+
+#pragma endregion
+
+
+#pragma region 인라인 함수
+	//함수를 호출하는 대신 함수가 호출되는 위치마다
+	//함수의 코드를 복사하여 전달하는 방식의 함수입니다.
+	int a = 3;
+	printf("%d\n",square(a));
+
+	//인라인 함수는 함수를 호출하는 과정이 없으므로 처리 속도가
+	//빠르지만, 인라인 함수를 많이 사용하게 되면 함수의 코드가
+	//복사되기 때문에 실제 파일의 크기가 커지게 됩니다.
+#pragma endregion
 
 
 }
