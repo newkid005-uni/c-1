69d52d8da9937ea0daf3e6a63767d41d525adf15
newkid005-uni <newkid005@gmail.com>
2026-01-05 21:43:48 +0900

    2026.01.05 구조체

--- c-2/Program_1/Source.c
+++ c-2/Program_1/Source.c
@@ -1,30 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 
-void main()
+struct Unit
 {
-#pragma region 동적 할당
-	//프로그램을 실행하는 중에 필요한 만큼 메모리를 할당
-	//하는 작업입니다.
+	char grade;
+	int health;
+	double experience;
+};
 
-	//int * x = malloc(sizeof(int));
-	//*x = 99;
-	//
-	//printf("%d\n", *x);
-	//
-	//free(x);
-	//
-	//printf("%d\n", *x);
 
-	//동적 할당은 실행 시간에 가변적으로 메모리의 크기를
-	//변경시킬 수 있으며, 동적으로 메모리의 크기를 할당할 때
-	//바이트 단위로 지정합니다.
+int main()
+{
+#pragma region 구조체
+	//여러 개의 변수를 하나의 집합으로 구조화한 다음
+	//하나의 객체를 생성하는 것입니다.
 
-	//동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로,
-	//사용이 끝나면 직접 해제해주어야 합니다.
+	//struct Unit unit = {'F',10,3.14};
+	//
+	//printf("grade = %c\nhealth = %d\nexp = %.3lf\n\n", unit.grade, unit.health, unit.experience);
+	//
+	////구조체의 각 멤버는 구조체 선언에서 나타나는 순서대로 초기화되며,
+	////이 순서는 초기화 목록의 값들이 왼쪽에서부터 오른쪽으로 시작됩니다.
+	//
+	//unit.grade = 'A';
+	//unit.health = 100;
+	//unit.experience = 135.75;
+	//
+	//printf("grade = %c\nhealth = %d\nexp = %.3lf\n\n", unit.grade, unit.health, unit.experience);
+
+	//구조체를 선언하기 전에 구조체는 메모리 공간이 생성되지 않으므로,
+	//구조체 내부에 있는 데이터를 초기화할 수 있습니다.
 
 #pragma endregion
 
 
+	return 0;
 }
